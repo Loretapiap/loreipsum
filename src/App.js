e2fd7e53df28f0bf25d4731fd461b64dbf189e51
Lorena <lorena@communis.cl>
2021-03-29 23:53:44 -0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import happyhacking from "./assets/happy-hacking.png";
 import success from "./assets/success.png";
+import Navbar from "./components/navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          <span class="highlight">Lorena Tapia</span> entregable 1
+          <span className="highlight">Lorena Tapia</span> entregable 1
         </p>
         <p>
           <code>mar 27, 2021</code>
         </p>
       </header>
-      <body class="flex">
-        <div class="base-50">
-          <div class="window">
-            <div class="icons">
+      <body className="flex">
+        <div className="base-50">
+          <div className="window">
+            <div className="icons">
               <span></span>
             </div>
           </div>
-          <div class="content">
+          <div className="content">
             <img src={happyhacking} className="screen-1" alt="logo" />
           </div>
         </div>
-        <div class="base-50">
-          <div class="window">
-            <div class="icons">
+        <div className="base-50">
+          <div className="window">
+            <div className="icons">
               <span></span>
             </div>
           </div>
-          <div class="content">
+          <div className="content">
             <img src={success} className="screen-1" alt="logo" />
           </div>
         </div>
