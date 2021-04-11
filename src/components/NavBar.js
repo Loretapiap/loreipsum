84175902af7ddcb6c152ed940baea6d52158fc81
Lorena <lorena@communis.cl>
2021-04-10 21:31:29 -0400

    Change tailwind cdn to npm config

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Brand from "../assets/logo.png";
+import Cart from "../components/cart";
 import "../assets/navcss.css";
 
 export default function Navbar({ fixed }) {
@@ -13,7 +14,7 @@ export default function Navbar({ fixed }) {
               className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase "
               href="#pablo"
             >
-                <img src={Brand} className="logo" alt="logo loreipsum" />
+              <img src={Brand} className="logo" alt="logo loreipsum" />
             </a>
             <button
               className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
@@ -52,10 +53,13 @@ export default function Navbar({ fixed }) {
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
                   href="#pablo"
                 >
-                 Deco hogar
+                  Deco hogar
                 </a>
               </li>
             </ul>
+            <div class="order-2 md:order-3 flex items-center" id="nav-content">
+              <Cart/>             
+            </div>
           </div>
         </div>
       </nav>
