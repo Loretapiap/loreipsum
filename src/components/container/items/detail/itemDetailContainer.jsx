0bcd795405d83c483e24225875a21088c6d8d239
Lorena Tapia <loretapiap@gmail.com>
2021-06-11 22:10:19 -0400

    Add error message on product

--- src/components/container/items/detail/itemDetailContainer.jsx
+++ src/components/container/items/detail/itemDetailContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import ItemDetail from "./itemDetail";
 import { getFirestore } from "../../../../Firebase";
 
@@ -7,6 +7,7 @@ const ItemDetailContainer = () => {
   const { id } = useParams();
   const [item, setitem] = useState({});
   const [isLoading, setisLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const db = getFirestore();
@@ -19,33 +20,47 @@ const ItemDetailContainer = () => {
       .then((doc) => {
         if (!doc.exists) {
           setisLoading(false);
-          console.log(
-            "Este producto no existe :("
-          );
+          setError("Este producto no existe :(");
         } else {
           setitem({ id: doc.id, ...doc.data() });
         }
       })
       .catch((error) => {
-        console.log(
-          "Ups! Parece que hubo un error. Intenta recargar la página"
-        );
+        setError("Ups! Parece que hubo un error. Intenta recargar la página");
       })
       .finally(() => {
         setisLoading(false);
       });
   }, [id]);
-  if (isLoading === true) {
-    return (
-      <>
-        <div className="my-40 flex flex-col items-center justify-center">
-          <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+
+  return (
+    <>
+      {isLoading ? (
+        <>
+          <div className="my-40 flex flex-col items-center justify-center">
+            <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+          </div>
+        </>
+      ) : error ? (
+        <div className="bg-gradient-to-r from-gray-300 to-blue-200">
+          <div className="w-9/12 m-auto py-16 min-h-screen flex items-center justify-center">
+            <div className="bg-white shadow overflow-hidden sm:rounded-lg pb-8">
+              <div className="border-t border-gray-200 text-center pt-8">
+                <h1 className="text-4xl font-medium p-8">
+                  {error}
+                </h1>
+                <Link className="bg-gradient-to-r from-red-400 to-red-500 hover:from-red-500 hover:to-red-500 text-white font-semibold px-6 py-3 rounded-md" to="/">
+                  Volver al inicio
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
-      </>
-    );
-  } else {
-    return <ItemDetail item={item} setitem={setitem} />;
-  }
+      ) : (
+        <ItemDetail item={item} setitem={setitem} />
+      )}
+    </>
+  );
 };
 
 export default ItemDetailContainer;
